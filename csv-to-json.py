380d6b0e59120a1726bc56105b9cd95121b46b04
jdascanio <julian.dascanio@gmail.com>
2023-06-28 23:15:42 -0300

    camisetas

--- csv-to-json.py
+++ csv-to-json.py
@@ -2,7 +2,7 @@ import csv
 import json
 
 with open ('positions.csv', 'r') as f1:
-    csv_reader = csv.reader(f1, delimiter=';')
+    csv_reader = csv.reader(f1, delimiter=',')
     positionsJson = []
     for n in csv_reader:
        newLine = {"team":n[0],"pts":n[1],"dg":n[2],"pj":n[3],"pg":n[4],"pe":n[5],"pp":n[6],"gf":n[7],"ge":n[8],"img":n[9]}
--- js/partidos.js
+++ js/partidos.js
@@ -1,7 +1,7 @@
 fetch("partidos.json")
   .then((res) => res.json())
   .then((data) => {
-      let principal = document.getElementById('main')
+    let principal = document.getElementById("main");
     data.forEach((item) => {
       let elemento = document.createElement("div");
       elemento.classList = "container-fecha";
@@ -12,24 +12,38 @@ fetch("partidos.json")
       <div class="center-date">
       <h3>${date}</h3>
       </div>`;
+      const imagenes = {
+        "La Naranja Geriatrica": "./img/LNG.webp",
+        "Carlinga": "./img/carlinga.webp",
+        "Pablo & Los Sextos": "./img/generica.webp",
+        "Rejunte FC": "./img/generica.webp",
+        "Los PapiFootball": "./img/generica.webp",
+        "Los Suplentes": "./img/suplentes.webp",
+        "Whiscola F.C.C.": "./img/whiscola.webp",
+        "All Inclusive F.C.": "./img/inclusive.webp",
+        "Farola FC": "./img/farola.webp",
+        "La Scaloneta": "./img/scaloneta.webp",
+        "Sala Azul Remix": "./img/generica.webp",
+      };
 
       matches.forEach((match) => {
         const { teamA, teamB, time, viewer } = match;
         let newElemento = document.createElement("div");
         newElemento.classList = "container-match";
+        let imgA = imagenes[`${teamA}`];
+        let imgB = imagenes[`${teamB}`];
         newElemento.innerHTML = `<table class="match">
         <tr>
-            <td class="team">${teamA}</td>
+            <td class="team"><img class="camiseta partido" src=${imgA} alt="camiseta">${teamA}</td>
             <td class="score"></td>
             <td class="score"></td>
-            <td class="team">${teamB}</td>
+            <td class="team">${teamB}<img class="camiseta partido" src=${imgB} alt="camiseta"></td>
         </tr>
         </table>
         <p>${time} hs</p>
         <p class="viewer">veedor: ${viewer}</p>`;
-        elemento.append(newElemento)
+        elemento.append(newElemento);
       });
-      principal.append(elemento)
-      
+      principal.append(elemento);
     });
   });
